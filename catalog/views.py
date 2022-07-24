ace55bdeb8ae8af23b40a0bf05a6909f85fa6abd
yuliia00 <skaskevich2000@gmail.com>
2022-07-24 22:02:02 +0200

    create views and templates part1

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Author, Book, Publisher, Store
+
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def books_list(request):
+    books = Book.objects.all()
+    return render(request, 'catalog/books.html', {'books': books, })
+
+
+def authors_list(request):
+    authors = Author.objects.all()
+    return render(request, 'catalog/authors.html', {'authors': authors, })
+
+
+def stores_list(request):
+    stores = Store.objects.all()
+    return render(request, 'catalog/stores.html', {'stores': stores, })
+
+
+def publishers_list(request):
+    publishers = Publisher.objects.all()
+    return render(request, 'catalog/publishers.html', {'publishers': publishers, })
